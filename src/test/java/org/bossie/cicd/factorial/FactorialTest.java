a2539836d358fe508e483e25973422e1deb03901
Jan Van den bosch <jan@bossie.org>
2016-01-27 08:45:34 +0100

    added a test

--- src/test/java/org/bossie/cicd/factorial/FactorialTest.java
+++ src/test/java/org/bossie/cicd/factorial/FactorialTest.java
@@ -6,4 +6,11 @@ public class FactorialTest extends TestCase {
     public void testFactorial() {
         assertEquals(120, Factorial.factorial(5));
     }
+
+    public void testThrowsOnNegativeNumber() {
+        try {
+            Factorial.factorial(-1);
+            fail("expected an IllegalArgumentException");
+        } catch (IllegalArgumentException e) { /* test succeeded */ }
+    }
 }
